c89b1b577d736ffe2fec5ef8c20b4b1108c0d192
Tomeczqueee <0faighter123@gmail.com>
2020-03-05 01:13:09 +0100

    Updates

---
+++ precache-manifest.94d29b52886702e3277377afe2631a0e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "957e807e886d79939b72a304da04739b",
+    "url": "/ogioldat/portfolio/index.html"
+  },
+  {
+    "revision": "3203a5d1c5e7a5819eb6",
+    "url": "/ogioldat/portfolio/static/js/2.1372f55f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/ogioldat/portfolio/static/js/2.1372f55f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e691cb2f9f37c96a0272",
+    "url": "/ogioldat/portfolio/static/js/main.3d6c2cda.chunk.js"
+  },
+  {
+    "revision": "0a7d08cb23a961702bfc",
+    "url": "/ogioldat/portfolio/static/js/runtime-main.c8e59116.js"
+  },
+  {
+    "revision": "012f1bcb1d44b2aa732fc8c8f371dee0",
+    "url": "/ogioldat/portfolio/static/media/artistify.012f1bcb.png"
+  },
+  {
+    "revision": "bdbc5ec20efd6f7c4e3dae7530049ba2",
+    "url": "/ogioldat/portfolio/static/media/ecoData.bdbc5ec2.png"
+  },
+  {
+    "revision": "27916068877b660937323bc4d6cdd961",
+    "url": "/ogioldat/portfolio/static/media/eo.27916068.png"
+  },
+  {
+    "revision": "ae1a59dfb302976aec66069c1e6e3346",
+    "url": "/ogioldat/portfolio/static/media/foodOrderApp.ae1a59df.png"
+  },
+  {
+    "revision": "e7f11eead118fe6a85dcf66d3440e08f",
+    "url": "/ogioldat/portfolio/static/media/meImg.e7f11eea.jpg"
+  }
+]);
