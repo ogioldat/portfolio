a78dd3a4de41099466777c19d3b9e5b3585f587e
ogioldat <tomasz.ogiolda00@gmail.com>
2020-05-03 19:31:04 +0200

    routes change

--- src/routes/index.js
+++
@@ -1,4 +0,0 @@
-export const routes = {
-    root: '/',
-    home: '/home',
-};
--- src/views/Root.js
+++ src/views/Root.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { routes } from 'routes';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import MainTemplate from 'templates/MainTemplate';
 import HomeView from "views/HomeView";
@@ -8,8 +7,8 @@ const Root = () => (
   <Router>
     <MainTemplate>
       <Switch>
-        <Route exact path={ routes.home } component={ HomeView }/>
-        <Route exact path={ routes.root } render={ () => <Redirect to={ routes.home }/> }/>
+        <Route exact path={ '/home' } component={ HomeView }/>
+        <Route exact path={ '/' } render={ () => <Redirect to={ '/home' }/> }/>
       </Switch>
     </MainTemplate>
   </Router>
