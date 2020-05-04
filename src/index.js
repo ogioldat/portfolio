7b3e60571df6d13a113fc4f5ac07c99eb114734a
ogioldat <tomasz.ogiolda00@gmail.com>
2020-05-04 12:15:07 +0200

    App.css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './views/Root';
+import App from './App.css'
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render(<Root/>, document.getElementById('root'));
